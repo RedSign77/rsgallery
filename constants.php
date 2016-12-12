27874fba262d675b5076d225672e853d8b0b3081
Zoltán Németh <signred@gmail.com>
2016-12-12 21:46:14 +0100

    Initialize version 1.0

---
+++ constants.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nemeth.zoltan
+ * Date: 2016. 04. 14.
+ * Time: 14:56
+ */
+
+define('RSG_TBL_GALLERY', 'rsgallery');
+define('RSG_TBL_PICTURE', 'rspicture');
+
+define('RSG_PUBLIC_ROOT', 'galleries');
+define('RSG_ADMIN_ROOT', 'admin/galleries');
+
---
+++ js/rsgallery.js
@@ -0,0 +1,6 @@
+(function ($) {
+    $(document).delegate('*[data-toggle="lightbox"]', 'click', function (event) {
+        event.preventDefault();
+        $(this).ekkoLightbox();
+    });
+})(jQuery);
